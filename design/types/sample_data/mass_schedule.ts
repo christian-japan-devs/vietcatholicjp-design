bbdda12d86eccb58b1d9ace8fbce96e0f7fa327f
danghoanggeo <dang.hoang.geo@gmail.com>
2022-12-03 16:58:56 +0900

    update index page

--- design/types/sample_data/mass_schedule.ts
+++ design/types/sample_data/mass_schedule.ts
@@ -68,4 +68,372 @@ export const mass_schedule = [
         "province":"Hiroshima"
       }]
     },
+]
+
+
+export const mass_home_full_schedule = [
+  {
+    "title":"CHÚA NHẬT III MÙA VỌNG",
+    "date":"18/12/2022",
+    "time_schedule":[{
+      "time":"11:00",
+      "father":"Cha Trinh",
+      "church_name":"Chính Tòa Oita",
+      "province":"Oita"
+    },
+    {
+      "time":"11:00",
+      "father":"Cha Tường SVD",
+      "church_name":"Seto",
+      "province":"Aichi"
+    },
+    {
+      "time":"12:15",
+      "father":"Cha Tận Hiến SDB",
+      "church_name":"Kakegawa",
+      "province":"Shizuoka"
+    },{
+      "time":"12:00",
+      "father":"Cha Phú SDB",
+      "church_name":"Yamato",
+      "province":"Kanagawa"
+    },{
+      "time":"14:00",
+      "father":"Cha Phong SVD",
+      "church_name":"Seishin Toyota",
+      "province":"Aichi"
+    },{
+      "time":"14:00",
+      "father":"Cha Điềm SVD",
+      "church_name":"Ogaki",
+      "province":"Gifu"
+    },{
+      "time":"14:00",
+      "father":"Cha Sơn",
+      "church_name":"Atsugi",
+      "province":"Kanayama"
+    },{
+      "time":"14:00",
+      "father":"Cha Thân SJ",
+      "church_name":"Misasa",
+      "province":"Hiroshima"
+    },{
+      "time":"14:00",
+      "father":"Cha Tú CM",
+      "church_name":"Shimabara",
+      "province":"Nagasaki"
+    },{
+      "time":"14:00",
+      "father":"Cha Toàn SJ",
+      "church_name":"Hosoe",
+      "province":"Yamaguchi"
+    },{
+      "time":"14:00",
+      "father":"Cha Thuyên CM",
+      "church_name":"Daimyo",
+      "province":"Fukuoka"
+    },{
+      "time":"15:00",
+      "father":"Cha Minh Toàn SJ",
+      "church_name":"Inhaxio",
+      "province":"Yotsuya, Tokyo"
+    },{
+      "time":"15:00",
+      "father":"",
+      "church_name":"Mizonoguchi",
+      "province":"Tokyo"
+    },{
+      "time":"15:00",
+      "father":"Cha Thế",
+      "church_name":"Okayama",
+      "province":"Okayama"
+    },{
+      "time":"15:00",
+      "father":"Cha Tận Hiến SDB",
+      "church_name":"Kusanagi",
+      "province":"Shizuoka"
+    },{
+      "time":"15:30",
+      "father":"Cha Sắc",
+      "church_name":"Chính Tòa Tamatsukuri",
+      "province":"Osaka"
+    },{
+      "time":"15:30",
+      "father":"Cha Trinh",
+      "church_name":"Kashima",
+      "province":"Saga"
+    },{
+      "time":"16:00",
+      "father":"Cha Quốc Tiến",
+      "church_name":"Akune",
+      "province":"Kagoshima"
+    },{
+      "time":"17:00",
+      "father":"Cha Nhã SJ",
+      "church_name":"Asakusa",
+      "province":"Tokyo"
+    },{
+      "time":"18:00",
+      "father":"Cha Thuyên CM",
+      "church_name":"Mizumaki",
+      "province":"Fukuoka"
+    }
+    ]
+  },
+]
+
+export const mass_home_schedule =
+  {
+    "title":"CHÚA NHẬT III MÙA VỌNG",
+    "date":"18/12/2022",
+    "time_schedule":[{
+      "time":"11:00",
+      "father":"Cha Trinh",
+      "church_name":"Chính Tòa Oita",
+      "province":"Oita"
+    },
+    {
+      "time":"11:00",
+      "father":"Cha Tường SVD",
+      "church_name":"Seto",
+      "province":"Aichi"
+    },
+    {
+      "time":"12:15",
+      "father":"Cha Tận Hiến SDB",
+      "church_name":"Kakegawa",
+      "province":"Shizuoka"
+    },{
+      "time":"12:00",
+      "father":"Cha Phú SDB",
+      "church_name":"Yamato",
+      "province":"Kanagawa"
+    },{
+      "time":"14:00",
+      "father":"Cha Phong SVD",
+      "church_name":"Seishin Toyota",
+      "province":"Aichi"
+    },{
+      "time":"14:00",
+      "father":"Cha Điềm SVD",
+      "church_name":"Ogaki",
+      "province":"Gifu"
+    },{
+      "time":"14:00",
+      "father":"Cha Sơn",
+      "church_name":"Atsugi",
+      "province":"Kanayama"
+    }
+    ]
+}
+
+export const mass_full_schedule = [
+  {
+    "title":"Thánh Lễ Vọng Giáng Sinh",
+    "date":"24/12/2022",
+    "time_schedule":[{
+      "time":"19:30",
+      "father":"Cha Nhã SJ",
+      "church_name":"Đại học Sophia",
+      "province":"Yotsuya, Tokyo"
+    },
+    {
+      "time":"21:30",
+      "father":"Cha Minh Toàn SJ",
+      "church_name":"Kanazawa",
+      "province":"Aichi"
+    }]
+  },{
+    "title":"Thánh Lễ Giáng Sinh",
+    "date":"25/12/2022",
+    "time_schedule":[{
+      "time":"10:00",
+      "father":"Cha Nhã SJ",
+      "church_name":"Đại học Sophia",
+      "province":"Yotsuya, Tokyo"
+    },
+    {
+      "time":"11:00",
+      "father":"Cha Tận Hiến",
+      "church_name":"Mitakabara",
+      "province":"Hamamatsu"
+    },
+    {
+      "time":"11:00",
+      "father":"Cha Sắc",
+      "church_name":"Takatori",
+      "province":"Kobe"
+    },{
+      "time":"11:30",
+      "father":"Cha Thế",
+      "church_name":"Chính toà Hiroshima",
+      "province":"Hiroshima"
+    },{
+      "time":"12:00",
+      "father":"Cha Toàn SJ",
+      "church_name":"Toyama",
+      "province":"Toyama"
+    },{
+      "time":"12:00",
+      "father":"Cha Trinh",
+      "church_name":"Chính Tòa Oita",
+      "province":"Oita"
+    },{
+      "time":"13:00",
+      "father":"Cha Điềm SVD",
+      "church_name":"Komaki",
+      "province":"Aichi-Ken"
+    },{
+      "time":"13:00",
+      "father":"Cha Nhã SJ",
+      "church_name":"Sendai",
+      "province":"Miyagi"
+    },{
+      "time":"13:00",
+      "father":"Cha Minh Anh",
+      "church_name":"Chính Tòa Kagoshima",
+      "province":"Kagoshima"
+    },{
+      "time":"14:00",
+      "father":"Cha Thủy SDB",
+      "church_name":"Oratorio",
+      "province":"Chofu, Tokyo"
+    },{
+      "time":"14:00",
+      "father":"Cha Thành",
+      "church_name":"Imabari",
+      "province":"Ehime"
+    },{
+      "time":"14:00",
+      "father":"Cha Phong SVD",
+      "church_name":"Nanzan",
+      "province":"Nagoya"
+    },{
+      "time":"14:00",
+      "father":"Cha Tú CM",
+      "church_name":"Isahaya",
+      "province":"Nagasaki"
+    },{
+      "time":"14:00",
+      "father":"Cha Toàn SJ",
+      "church_name":"Ube",
+      "province":"Yamaguchi"
+    },{
+      "time":"14:00",
+      "father":"Cha Thuyên",
+      "church_name":"Daimyo",
+      "province":"Fukuoka"
+    },{
+      "time":"14:30",
+      "father":"Cha Quốc Tiến",
+      "church_name":"Sendai",
+      "province":"Kagoshima"
+    },{
+      "time":"15:00",
+      "father":"Cha Quang Thuần",
+      "church_name":"Fukuyama",
+      "province":"Hiroshima"
+    },{
+      "time":"15:30",
+      "father":"Cha Lợi",
+      "church_name":"nhà chung Thánh Tâm Chúa Giêsu",
+      "province":"Okayama"
+    },{
+      "time":"15:30",
+      "father":"Cha Năng CMF",
+      "church_name":"Chính Tòa Tamatsukuri",
+      "province":"Osaka"
+    },{
+      "time":"16:30",
+      "father":"Cha Trinh",
+      "church_name":"Josuidori",
+      "province":"Fukuoka"
+    },{
+      "time":"16:30",
+      "father":"Cha Tƣờng SVD",
+      "church_name":"Kariya",
+      "province":"Aichi"
+    }
+    ]
+  },{
+    "title":"Thánh Lễ Cuối Năm",
+    "date":"31/12/2022",
+    "time_schedule":[{
+      "time":"19:00",
+      "father":"Cha Sắc",
+      "church_name":"Kakogawa",
+      "province":"Hyogo"
+    },
+    {
+      "time":"19:00",
+      "father":"Cha Trinh",
+      "church_name":"Miyakonojo",
+      "province":"Miyzaki"
+    }]
+  },{
+    "title":"Thánh Lễ Đầu Năm Mới",
+    "date":"01/01/2023",
+    "time_schedule":[{
+      "time":"12:00",
+      "father":"Cha Trinh",
+      "church_name":"Miyakonojo",
+      "province":"Miyzaki"
+    },{
+      "time":"13:00",
+      "father":"",
+      "church_name":"Kariya",
+      "province":"Aichi"
+    },{
+      "time":"13:00",
+      "father":"",
+      "church_name":"Ichinomiya",
+      "province":"Aichi"
+    },{
+      "time":"14:00",
+      "father":"Cha Quang Thuần",
+      "church_name":"Higashi",
+      "province":"Hiroshima"
+    },{
+      "time":"14:00",
+      "father":"Cha Toàn SJ",
+      "church_name":"Hosoe",
+      "province":"Yamaguchi"
+    },{
+      "time":"14:00",
+      "father":"Cha Minh Anh",
+      "church_name":"Taniyama",
+      "province":"Kagoshima"
+    },{
+      "time":"15:00",
+      "father":"Cha Bỉnh OFM",
+      "church_name":"Nigawa",
+      "province":"Osaka"
+    },{
+      "time":"15:00",
+      "father":"Cha Nhã SJ",
+      "church_name":"Inhaxio",
+      "province":"Yotsuya, Tokyo"
+    },{
+      "time":"15:00",
+      "father":"Cha Thuyên CM",
+      "church_name":"Kurume",
+      "province":"Fukuoka"
+    },{
+      "time":"15:30",
+      "father":"Cha Sắc",
+      "church_name":"Chính Tòa Tamatsukuri",
+      "province":"Osaka"
+    },{
+      "time":"16:00",
+      "father":"Cha Tâm MF",
+      "church_name":"Fujisawa",
+      "province":"Yokohama"
+    },{
+      "time":"17:00",
+      "father":"",
+      "church_name":"Narashino",
+      "province":"Chiba"
+    }
   ]
+  },
+]
