3ee23d393c736f1596633cbc2ef00b2f44e94cc5
danghoanggeo <dang.hoang.geo@gmail.com>
2022-12-10 23:11:49 +0900

    added drawer

--- design/components/layout/navbar.tsx
+++ design/components/layout/navbar.tsx
@@ -38,14 +38,14 @@ export default function NavBar() {
           </defs>
         </svg>
       </div>
-      <div className="px-6 pt-6 lg:px-8">
+      <div className="px-6 pt-4 lg:px-8">
         <div>
           <nav className="flex h-9 items-center justify-between" aria-label="Global">
             <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
               <Link href="/" legacyBehavior>
-                <a  className="-m-1.5 mb-2 p-1.5 xl:ml-12 xl:mb-4">
-                  <span className="sr-only">VietCatholicJapan</span>
-                  <img className="h-8" src="/viet-catholicjp-256.svg" alt="vietcatholicjp" />
+                <a className="flex items-center">
+                    <img src="/viet-catholicjp-256.svg" className="mr-3 h-6 sm:h-9" alt="Viet Catholic Japan" />
+                    <span className="self-center text-md md:text-xl font-semibold whitespace-nowrap dark:text-white">VietCatholicJapan</span>
                 </a>
               </Link>
             </div>
@@ -79,43 +79,41 @@ export default function NavBar() {
           </nav>
           <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
             <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white dark:bg-slate-900 px-6 py-6 lg:hidden">
-            <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-              <svg
-                className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-                viewBox="0 0 1155 678"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
-                  fillOpacity=".3"
-                  d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-                />
-                <defs>
-                  <linearGradient
-                    id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
-                    x1="1155.49"
-                    x2="-78.208"
-                    y1=".177"
-                    y2="474.645"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop stopColor="#9089FC" />
-                    <stop offset={1} stopColor="#FF80B5" />
-                  </linearGradient>
-                </defs>
-              </svg>
-            </div>
+              <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+                <svg
+                  className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+                  viewBox="0 0 1155 678"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+                    fillOpacity=".3"
+                    d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+                      x1="1155.49"
+                      x2="-78.208"
+                      y1=".177"
+                      y2="474.645"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stopColor="#9089FC" />
+                      <stop offset={1} stopColor="#FF80B5" />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
               <div className="flex h-9 items-center justify-between">
-                <div className="flex">
-                  <a href="#" className="-m-1.5 p-1.5">
-                    <span className="sr-only">VietCatholicJapan</span>
-                    <img
-                      className="h-8"
-                      src="/viet-catholicjp-256.svg"
-                      alt="vietcatholicjp"
-                    />
-                  </a>
+                <div>
+                  <Link href="/" legacyBehavior>
+                    <a className="flex items-center">
+                        <img src="/viet-catholicjp-256.svg" className="mr-3 h-6 sm:h-9" alt="Viet Catholic Japan" />
+                        <span className="self-center text-md md:text-xl font-semibold whitespace-nowrap dark:text-white">VietCatholicJapan</span>
+                    </a>
+                  </Link>
                 </div>
                 <div className="flex">
                   <button
